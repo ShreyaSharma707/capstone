5c2aa1fdc474add1afc7966ece222b2a399e2ea2
ShreyaSharma707 <142740086+ShreyaSharma707@users.noreply.github.com>
2026-01-21 00:37:48 +0530

    Delete

--- coastalwatch-backend/src/controllers/sensorController.js
+++
@@ -1,8 +0,0 @@
-import Sensor from '../models/Sensor.js';
-
-export async function listSensors(req, res) {
-  const sensors = await Sensor.find({}).sort({ timestamp: -1 }).limit(200);
-  return res.json(sensors);
-}
-
-
--- coastalwatch-backend/src/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-import User from '../models/User.js';
-
-export async function listUsers(req, res) {
-  const users = await User.find({}, { password: 0 }).sort({ createdAt: -1 });
-  return res.json(users);
-}
-
-
